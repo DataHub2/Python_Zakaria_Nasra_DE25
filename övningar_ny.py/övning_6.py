40add7066dee1922583d5d9d4b493f0e1253c62d
DataHub2 <zakariaallmaninfo@gmail.com>
2025-10-23 12:13:27 +0200

    skapat ny mapp för lab 2

---
+++ övningar_ny.py/övning_6.py
@@ -0,0 +1,36 @@
+from numbers import Number
+
+
+class UnitConverter:
+    # | means or
+    def __init__(self, value: int | float):
+        self.value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value):
+        # validation code
+        if not isinstance(new_value, Number):
+            raise TypeError(f"the value must be a Number not a {type(new_value)}")
+
+        if new_value < 0:
+            raise ValueError(f"value must be larger than 0 not {new_value}")
+
+        self._value = new_value
+
+
+unit_converter = UnitConverter(5)
+
+# shortcut to get this output:
+# unit_converter.value = 5
+print(f"{unit_converter.value = }")
+
+try:
+    unit_converter.value = "5"
+except TypeError as err:
+    print(err)
+
+
