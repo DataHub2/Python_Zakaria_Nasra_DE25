6716fd3c46ef561c492c6ea5d8f772c9d22aec1f
DataHub2 <zakariaallmaninfo@gmail.com>
2025-10-28 03:27:17 +0100

    i have now started with property and will carry on later, i defiend both area and perimeter. and i made a raise notimplementedError, så that the underclasses kan make versions of area and perimeter

--- lab2.py/shape.py
+++ lab2.py/shape.py
@@ -9,8 +9,23 @@ class Shape:
             raise TypeError(f"y has to be a number")
         
         # this is going to store the values of x and y as float values, beacuse the positions have decimal in them 
-        self._x = float # this is privat beacause i would like to control how the values are set in the code. 
-        self._y = float 
+        self._x = float(x) # this is privat beacause i would like to control how the values are set in the code. 
+        self._y = float(y)
+
+
+        # this lines bellow will create property thst is a read only property 
+        @property
+        def area(self):
+            raise NotImplementedError("the underclass has to be implemented for area") # the area method has to ba used in the underclass 
+        
+        
+        @property
+        def perimeter(self):
+            raise NotImplementedError("the underclass has to be implemented for perimeter") # the perimeter method has to ba used in the underclass
+    
+        
+
+            
 
         
       
