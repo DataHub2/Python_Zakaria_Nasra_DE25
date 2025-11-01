eb416f6805a42a63b294dd15cea41e7f0151dc59
DataHub2 <zakariaallmaninfo@gmail.com>
2025-11-01 18:16:17 +0100

    i have now added the width and the height with the initializer now that it is defiend i have also put in the super function that will call to the parent shape and give accses to the methods and properies

--- lab2_map.py/rectangle.py
+++ lab2_map.py/rectangle.py
@@ -3,5 +3,11 @@ import math
 
 class Rectangle(Shape):
 
-    def __init__ (self, x = 0, y = 0):
+    #width and height had to be set to a defult = 1 so thst i got be able to work with it.
+    def __init__ (self, x = 0, y = 0, width = 1, height = 1 ):
+        # this will connect with the parent class
+        super().__init__(x, y)  
+
+
+        
         
