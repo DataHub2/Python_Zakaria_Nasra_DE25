72ec4a475bff966e39309083793f13fd78c9ed8e
DataHub2 <zakariaallmaninfo@gmail.com>
2025-11-03 21:10:43 +0100

    i added the super function so that i can call to the parent class

--- lab2_map.py/circle.py
+++ lab2_map.py/circle.py
@@ -1,7 +1,14 @@
 from shape import Shape 
 import math 
-
+# this is ht e child class of Shape, this class is a circle.
 class Circle(Shape):
-
+    #this class is for creating a circle position on a coordinate system.
     def __init__(self, x = 0, y = 0, radius = 1):
-        
+
+        super().__init__(x, y) # this is to connect with the parent class
+
+
+    
+
+    
+
